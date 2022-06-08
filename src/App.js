de96147808e66068184abee15ac5a79262550548
seeoya <seeoya@naver.com>
2022-06-08 18:40:11 +0900

    react-bootstrap 설치, 이미지 업로드

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+// react-bootstrap 홈페이지에서 npm으로 install
+// 대문자 컴포넌트 (Button, Navbar, Nav 등등...)는 import 필요
+import { Button, Navbar, Container, Nav, Row, Col } from "react-bootstrap";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar bg="dark" variant="dark" sticky="top">
+                <Container>
+                    <Navbar.Brand href="#home">쇼핑몰 DEMO</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">a</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+            <div className="main-banner"></div>
+
+            <Container>
+                <Row>
+                    <Col sm={8}>sm=8</Col>
+                    <Col sm={4}>sm=4</Col>
+                </Row>
+                <Row>
+                    <Col sm>sm=true</Col>
+                    <Col sm>sm=true</Col>
+                    <Col sm>sm=true</Col>
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
