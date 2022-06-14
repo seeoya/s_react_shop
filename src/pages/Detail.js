4354ad54d5b771961999f61385450f07c37dbb63
seeoya <seeoya@naver.com>
2022-06-14 16:22:28 +0900

    detail 페이지 탭 추가, state로 탭 변경

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Nav, Link } from "react-bootstrap";
 import styled, { css } from "styled-components";
 
 let _PUBLIC_URL = process.env.PUBLIC_URL;
@@ -40,24 +41,30 @@ let DdBtn = styled(DfBtn)`
 function Detail(props) {
     let [ttestt, setTtestt] = useState(true);
     let [iValue, setIValue] = useState("");
+    let [tab, setTab] = useState(0);
 
     useEffect(() => {
         // mount, update 될 때 (렌더링 될 때) 실행됨.
         // html 렌더링 이후에 작동.
         // 어려운 연산, 서버에서 데이터 가져오기, 타이머 등 여기에 적으면 좋음
 
-        setTimeout(() => {
-            // document.getElementsByClassName("ttestt")[0].style.display = "none";
-            // setTtestt(false);
-            console.log("실행");
-        }, 0);
+        if (ttestt) {
+            setTimeout(() => {
+                let box = document.getElementsByClassName("ttestt")[0];
+                if (box) {
+                    box.style.display = "none";
+                    setTtestt(false);
+                    console.log("use Effect 실행");
+                }
+            }, 1000);
+        }
 
         // clean up. 아래 return은 useEffect 실행되기 전에 실행됨.
         return () => {
             // 클린업 함수 위치
             // 기존 타이머 제거하는 등.
             // 초기 mount 시에는 실행 안 되지만 unmount 시에는 실행됨
-            console.log("리턴");
+            console.log("리턴 먼저 실행");
         };
     }, [ttestt]);
     // dependency
@@ -81,11 +88,12 @@ function Detail(props) {
         return (
             <>
                 <div>{iValue}</div>
-                <input type="text" onChange={changeIValue} value={iValue} />
-                <button onClick={() => setTtestt(!ttestt)}>{ttestt ? "끕니다" : "켭니다"}</button>
+                <input type="text" placeholder="숫자만 입력하세요." onChange={changeIValue} value={iValue} />
+                <br />
+                <button onClick={() => setTtestt(!ttestt)}>박스를 {ttestt ? "끕니다" : "켭니다"}</button>
                 {ttestt ? (
                     <div className="ttestt" style={{ background: "yellow" }}>
-                        1214234
+                        시간제 or 토글 박스
                     </div>
                 ) : null}
 
@@ -112,6 +120,27 @@ function Detail(props) {
                         </div>
                     </div>
                 </div>
+
+                <Nav variant="pills" defaultActiveKey="link0">
+                    <Nav.Item>
+                        <Nav.Link onClick={() => setTab(0)} eventKey="link0">
+                            Active
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link onClick={() => setTab(1)} eventKey="link1">
+                            Option 2
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link onClick={() => setTab(2)} eventKey="link2">
+                            Option 3
+                        </Nav.Link>
+                    </Nav.Item>
+                </Nav>
+
+                {/* if문을 컴포넌트로 분리할 수도 있음  */}
+                <TabInner tab={tab} />
             </>
         );
     } else {
@@ -125,6 +154,24 @@ function Detail(props) {
     function changeIValue(e) {
         setIValue(e.target.value);
     }
+}
+
+// props 재지정이 귀찮으면 바로 parameter에서 {} 로 props 가져올 수 있음
+function TabInner({ tab }) {
+    // let { tab } = props;
+
+    // switch (tab) {
+    //     case 0:
+    //         return <div>내용0</div>;
+    //     case 1:
+    //         return <div>내용1</div>;
+    //     case 2:
+    //         return <div>내용2</div>;
+    // }
+
+    // array 이용해서 이런 식으로도 되겠네요
+    // 생각 못했네...
+    return [<div>내용0입니다</div>, <div>내용1입니다</div>, <div>내용2라네요</div>][tab];
 }
 
 export default Detail;
